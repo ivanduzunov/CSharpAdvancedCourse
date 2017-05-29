b5a74c0982af2a8624e96b10e7748a922a3cf7e2
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-05-30 00:17:08 +0300

    s

---
+++ Bashsoft/StartUp/DirectoryInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUp
+{
+    public static class DirectoryInfo
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+
+            }
+
+        }
+    }
+}
