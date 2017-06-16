8bad6d6dd9b5f3b4fbb642521b9b02896e0e851a
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-06-16 22:50:25 +0300

    regex

---
+++ RegularExpressions/08.ExtractHyperlinks/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _08.ExtractHyperlinks //83/100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            while (input != "END")
+            {
+                sb.Append(input).Append(" ");
+                input = Console.ReadLine();
+            }
+
+
+
+            string pattern = @"<a\s+[^>]*?href\s*=(.*?)>.*?<\s*\/\s*a\s*>";
+
+            MatchCollection collection = Regex.Matches(sb.ToString(), pattern);
+
+            foreach (Match match in collection)
+            {
+                var notClean = (match.Groups[1].Value.ToString().Trim());
+
+                if (notClean[0] == '"')
+                {
+                    Console.WriteLine(notClean.Split(new[] { '"' }, StringSplitOptions.RemoveEmptyEntries).First());
+                }
+                else if (notClean[0] == '\'')
+                {                  
+                    Console.WriteLine(notClean.Split(new[] { '\'' }, StringSplitOptions.RemoveEmptyEntries).First());
+                }
+                else if (notClean[0] == ' ')
+                {
+                    Console.WriteLine(notClean.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).First());
+                }
+
+            }
+        }
+    }
+}
---
+++ RegularExpressions/09.QueryMess/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _09.QueryMess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //45/100
+            var line = Console.ReadLine();
+
+            List<string> resultList = new List<string>();
+            while (line != "END")
+            {
+                MatchCollection collection = Regex.Matches(line, @"(\w+)[+=].*?[+%20\w/:.*]+");
+                Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+                foreach (Match match in collection)
+                {
+                    var pair = match.ToString().Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                    var field = Regex.Match(pair[0], @"\w+").ToString();
+                    var values = pair[1].Split(new[] { "%20" }, StringSplitOptions.RemoveEmptyEntries).Select(w => Regex.Replace(w, "[+]+", " ")).Select(w => w.Trim()).ToList();
+
+                    FillTheDictionary(dict, field, values);
+                }
+                StringBuilder sb = new StringBuilder();
+                foreach (var pair in dict)
+                {
+                    sb.Append($"{pair.Key}=[{string.Join(", ", pair.Value)}]");
+                }
+                resultList.Add(sb.ToString());
+
+                line = Console.ReadLine();
+            }
+            foreach (var item in resultList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void FillTheDictionary(Dictionary<string, List<string>> dictionary, string field, List<string> valuesList)
+        {
+            if (!dictionary.ContainsKey(field))
+            {
+                dictionary.Add(field, new List<string>());
+            }
+
+            foreach (var value in valuesList)
+            {
+                dictionary[field].Add(value);
+            }
+        }
+
+
+    }
+}
