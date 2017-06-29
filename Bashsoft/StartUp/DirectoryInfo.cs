aa35a7299d25bad206e69853c63c46901e867632
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-06-29 17:26:36 +0300

    first

--- Bashsoft/StartUp/DirectoryInfo.cs
+++ Bashsoft/StartUp/DirectoryInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,6 @@ namespace StartUp
 {
     public static class DirectoryInfo
     {
-        public static void TraverseDirectory(string path)
-        {
-            OutputWriter.WriteEmptyLine();
-            int initialIdentation = path.Split('\\').Length;
-            Queue<string> subFolders = new Queue<string>();
-            subFolders.Enqueue(path);
-
-            while (subFolders.Count != 0)
-            {
-                string currentPath = subFolders.Dequeue();
-               
-               
-            }
-
-        }
+        
     }
 }
--- Bashsoft/StartUp/IOManager.cs
+++ Bashsoft/StartUp/IOManager.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using StartUp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StartUp
+namespace BashSoft
 {
     public static class IOManager
     {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identetion = currentPath.Split('\\').Length - initialIdentation;
+
+                OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}",
+                    new string('-', identetion),
+                    currentPath));
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+
+            }
+
+        }
     }
 }
