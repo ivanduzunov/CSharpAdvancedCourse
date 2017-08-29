47e0682dd8d5f16874e8d5e6a15dc1a69975b173
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-08-29 21:09:44 +0300

    g

--- ExamJune2017/01.Regeh/StartUp.cs
+++ ExamJune2017/01.Regeh/StartUp.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _01.Regeh
 {
@@ -10,8 +9,51 @@ namespace _01.Regeh
     {
         public static void Main(string[] args)
         {
-            var input = Console.ReadLine();
-            //  .*\[([\x00-\x7F]+)<(\d+)REGEH(\d+)>?([\x00-\x7F]+)\]
+            var input = Console.ReadLine().Trim();
+            var pattern = @"\[(\w+)<(\d+)REGEH(\d+)>(\w+)\]";
+            MatchCollection collection = Regex.Matches(input, pattern);
+            List<int> numbers = ExtractNumbers(collection);
+
+            if (numbers.Count > 0)
+            {
+                string result = ExtractString(input, numbers);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static string ExtractString(string input, List<int> numbers)
+        {
+            StringBuilder sb = new StringBuilder();
+            int previousIndexesSum = 0;
+            int inputLenght = input.Length;
+
+            foreach (int num in numbers)
+            {
+                previousIndexesSum += num;
+
+                if (previousIndexesSum > inputLenght)
+                {
+                    int helper = (previousIndexesSum / inputLenght);
+                    var index = (previousIndexesSum - (helper * inputLenght)) - 1;
+                    sb.Append(input[index]);
+                }
+                else
+                {
+                    sb.Append(input[previousIndexesSum]);
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+        private static List<int> ExtractNumbers(MatchCollection collection)
+        {
+            List<int> result = new List<int>();
+            foreach (Match match in collection)
+            {
+                result.Add(int.Parse(match.Groups[2].Value));
+                result.Add(int.Parse(match.Groups[3].Value));
+            }
+            return result;
         }
     }
 }
--- ExamJune2017/03. Number Wars/Program.cs
+++ ExamJune2017/03. Number Wars/Program.cs
@@ -6,10 +6,203 @@ using System.Threading.Tasks;
 
 namespace _03.Number_Wars
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var firstPlayerInput = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var secondPlayerInput = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Queue<Card> firstPlayerCards = AddPlayerCards(firstPlayerInput);
+            Queue<Card> secondPlayerCards = AddPlayerCards(secondPlayerInput);
+            bool gameOver = false;
+            bool IsDraw = false;
+            int turns = 0;
+            string playerWinner = string.Empty;
+            var deck = new List<Card>();
+
+            while (gameOver == false)
+            {
+                Card firstPlayerCard = firstPlayerCards.Dequeue();
+                Card secondPlayerCard = secondPlayerCards.Dequeue();
+
+                deck.Add(firstPlayerCard);
+                deck.Add(secondPlayerCard);
+
+                if (firstPlayerCard.Number > secondPlayerCard.Number)
+                {
+                    TransferCardsFromDeckToPerson(firstPlayerCards, deck);
+                }
+                else if (firstPlayerCard.Number < secondPlayerCard.Number)
+                {
+                    TransferCardsFromDeckToPerson(secondPlayerCards, deck);
+                }
+                else
+                {
+                    bool thereIsWinner = false;
+
+                    while (thereIsWinner == false)
+                    {
+                        Card[] fpCards = new Card[3].ToArray();
+
+                        if (firstPlayerCards.Count < 3)
+                        {
+                            for (int i = 0; i < firstPlayerCards.Count; i++)
+                            {
+                                fpCards[i] = firstPlayerCards.Dequeue();
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                fpCards[i] = firstPlayerCards.Dequeue();
+                            }
+                        }
+
+
+                        Card[] spCards = new Card[3].ToArray();
+
+                        if (secondPlayerCards.Count < 3)
+                        {
+                            for (int i = 0; i < secondPlayerCards.Count; i++)
+                            {
+                                spCards[i] = secondPlayerCards.Dequeue();
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                spCards[i] = secondPlayerCards.Dequeue();
+                            }
+                        }
+
+
+                        for (int i = 0; i < fpCards.Length; i++)
+                        {
+                            deck.Add(fpCards[i]);
+                        }
+                        for (int i = 0; i < spCards.Length; i++)
+                        {
+                            deck.Add(spCards[i]);
+                        }
+
+
+
+
+                        int firstPlayerSum = fpCards.Sum(c => c.LetterValue);
+                        int secondPlayerSum = spCards.Sum(c => c.LetterValue);
+
+                        if (firstPlayerSum > secondPlayerSum)
+                        {
+                            TransferCardsFromDeckToPerson(firstPlayerCards, deck);
+                            thereIsWinner = true;
+                        }
+                        else if (firstPlayerSum < secondPlayerSum)
+                        {
+                            TransferCardsFromDeckToPerson(secondPlayerCards, deck);
+                            thereIsWinner = true;
+                        }
+                        if (firstPlayerCards.Count == 0 && secondPlayerCards.Count == 0)
+                        {
+                            thereIsWinner = true;
+                            gameOver = true;
+                            IsDraw = true;
+                        }
+                    }
+                }
+
+                if (firstPlayerCards.Count == 0)
+                {
+                    gameOver = true;
+                    playerWinner = "Second";
+                }
+                else if (secondPlayerCards.Count == 0)
+                {
+                    gameOver = true;
+                    playerWinner = "First";
+                }
+
+
+
+                turns++;
+
+                if (turns == 1000000)
+                {
+                    if (secondPlayerCards.Count > firstPlayerCards.Count)
+                    {
+                        gameOver = true;
+                        playerWinner = "Second";
+                    }
+                    else
+                    {
+                        gameOver = true;
+                        playerWinner = "First";
+                    }
+                }
+            }
+            if (IsDraw)
+            {
+                Console.WriteLine($"Draw after {turns} turns");
+            }
+            else if (playerWinner == "First")
+            {
+                Console.WriteLine($"First player wins after {turns} turns");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins after {turns} turns");
+            }
         }
+
+        private static void TransferCardsFromDeckToPerson(Queue<Card> firstPlayerCards,
+            List<Card> deck)
+        {
+            foreach (var card in deck.OrderByDescending(c => c.Number).ThenByDescending(c => c.LetterValue))
+            {
+                firstPlayerCards.Enqueue(card);
+            }
+            deck.Clear();
+        }
+
+
+        private static Queue<Card> AddPlayerCards(string[] playerInput)
+        {
+            Queue<Card> cards = new Queue<Card>();
+
+            foreach (var cardInput in playerInput)
+            {
+                int index = 0;
+                for (int i = 0; i < cardInput.Length; i++)
+                {
+                    if (!char.IsDigit(cardInput[i]))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+
+                int number = int.Parse(cardInput.Substring(0, index));
+                char letter = char.Parse(cardInput[index].ToString());
+
+                cards.Enqueue(new Card(number, letter));
+            }
+
+            return cards;
+        }
+    }
+
+    public class Card
+    {
+        public Card(int number, char letter)
+        {
+            Number = number;
+            LetterValue = char.ToUpper(letter) - 64;
+        }
+
+        public int Number { get; set; }
+        public int LetterValue { get; set; }
     }
 }
