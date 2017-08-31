8b138f3128f20efc678043d90f831010a951f74c
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-08-31 20:44:52 +0300

    d

---
+++ Exam13June2016/01.JediMeditation/StartUp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.JediMeditation
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> masters = new Queue<string>();
+            Queue<string> knights = new Queue<string>();
+            Queue<string> padwans = new Queue<string>();
+            Queue<string> ourGuys = new Queue<string>();
+            bool yodaIsPresent = false;
+             
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                tokens.ForEach(t =>
+                {
+                    if (t[0] == 'm') masters.Enqueue(t);
+                    else if (t[0] == 'k') knights.Enqueue(t);
+                    else if (t[0] == 'p') padwans.Enqueue(t);
+                    else if (t[0] == 't' || t[0] == 's') ourGuys.Enqueue(t);
+                    else yodaIsPresent = true;
+                });
+            }
+            string output = Output(masters, knights, padwans, ourGuys, yodaIsPresent);
+            Console.WriteLine(output);
+        }
+
+        private static string Output(
+            Queue<string> masters, 
+            Queue<string> knights,
+            Queue<string> padwans,
+            Queue<string> ourGuys,
+            bool yodaIsPresent)
+        {
+            StringBuilder sb = new StringBuilder();
+            int counter = 0;
+
+            if (yodaIsPresent)
+            {
+                counter = masters.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(masters.Dequeue() + " ");
+                }
+                counter = knights.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(knights.Dequeue() + " ");
+                }
+                counter = ourGuys.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(ourGuys.Dequeue() + " ");
+                }
+                
+            }
+            else
+            {
+                counter = ourGuys.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(ourGuys.Dequeue() + " ");
+                }
+                counter = masters.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(masters.Dequeue() + " ");
+                }
+                counter = knights.Count;
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(knights.Dequeue() + " ");
+                }
+            }
+            counter = padwans.Count;
+            for (int i = 0; i < counter; i++)
+            {
+                sb.Append(padwans.Dequeue() + " ");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Exam13June2016/02.JediGalaxy/StartUp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.JediGalaxy
+{
+    // 80/100 The last two tests - "Time Limit"
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            long[][] galaxy = CreateTheGalaxy(Console.ReadLine().Split(' '));
+            var input = string.Empty;
+            long jediPoints = 0;
+
+            while ((input = Console.ReadLine()) != "Let the Force be with you")
+            {
+                var jediCoordinates = input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var sithCoordinates = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                int jediInitialRow = int.Parse(jediCoordinates[0]);
+                int jediInitialCol = int.Parse(jediCoordinates[1]);
+                int sithInitialRow = int.Parse(sithCoordinates[0]);
+                int sithInitialCol = int.Parse(sithCoordinates[1]);
+
+                SithDestructionOfPlanets(galaxy, sithInitialRow, sithInitialCol);
+                jediPoints += JediCollectingOfPoints(galaxy, jediInitialRow, jediInitialCol);
+            }
+            Console.WriteLine(jediPoints);
+        }
+
+        private static long JediCollectingOfPoints(long[][] galaxy, int row, int col)
+        {
+            long jediPoints = 0;
+            for (long i = row; i >= 0; i--)
+            {
+                if (PlanetIsInTheGalaxy(i, col, galaxy.Length, galaxy[0].Length))
+                {
+                    jediPoints += galaxy[i][col];
+                }
+                col++;
+            }
+            return jediPoints;
+        }
+
+        private static void SithDestructionOfPlanets(long[][] galaxy, int row, int col)
+        {
+            for (long i = row; i >= 0; i--)
+            {
+                if (PlanetIsInTheGalaxy(i, col, galaxy.Length, galaxy[0].Length))
+                {
+                    galaxy[i][col] = 0;
+                }
+                col--;
+            }
+        }
+
+        private static bool PlanetIsInTheGalaxy(long i, long j, long galaxyLength, long lenght)
+        {
+            bool isValidRow = i < galaxyLength && i >= 0;
+            bool isValidCol = j < lenght && j >= 0;
+            return isValidCol && isValidRow;
+        }
+
+        private static long[][] CreateTheGalaxy(string[] longs)
+        {
+            long rows = long.Parse(longs[0]);
+            long cols = long.Parse(longs[1]);
+            long[][] toReturn = new long[rows][];
+            long counter = 0;
+
+            for (long i = 0; i < rows; i++)
+            {
+                toReturn[i] = new long[cols];
+                for (long j = 0; j < cols; j++)
+                {
+                    toReturn[i][j] = counter;
+                    counter++;
+                }
+            }
+            return toReturn;
+        }
+    }
+}
