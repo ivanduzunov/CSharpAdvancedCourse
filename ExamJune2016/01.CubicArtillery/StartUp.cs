74771afa9012fd3a9f3d84d4d497c835e65e9e35
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-09-02 19:28:56 +0300

    дсдс

--- ExamJune2016/01.CubicArtillery/StartUp.cs
+++ ExamJune2016/01.CubicArtillery/StartUp.cs
@@ -1,35 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _01.CubicArtillery
+﻿namespace Problem_1
 {
-    public class StartUp
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    class Startup
     {
-        public static void Main(string[] args)
+        static void Main()
         {
+            int maxCapacity = int.Parse(Console.ReadLine());
+
             var bunkers = new Queue<string>();
             var weapons = new Queue<int>();
-            int freeCapacity = int.Parse(Console.ReadLine());
-            string input = string.Empty;
+            int freeCapacity = maxCapacity;
 
-            while ((input = Console.ReadLine()) != "Bunker Revision")
+            Regex rgx = new Regex("[a-zA-Z]");
+
+            string input = Console.ReadLine();
+            while (input != "Bunker Revision")
             {
-                string[] tokens = input.Split(new [] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                string[] tokens = input.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-                foreach (var element in tokens)
+                foreach (var item in tokens)
                 {
-                    if (!char.IsDigit(element[0]))
+                    if (rgx.IsMatch(item))
                     {
-                        bunkers.Enqueue(element);
+                        bunkers.Enqueue(item);
                     }
                     else
                     {
-                        
+                        int weaponCapacity = int.Parse(item);
+
+                        bool weaponContained = false;
+                        while (bunkers.Count > 1)
+                        {
+                            if (freeCapacity >= weaponCapacity)
+                            {
+                                weapons.Enqueue(weaponCapacity);
+                                freeCapacity -= weaponCapacity;
+                                weaponContained = true;
+                                break;
+                            }
+
+                            if (weapons.Count == 0)
+                            {
+                                Console.WriteLine($"{bunkers.Peek()} -> Empty");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{bunkers.Peek()} -> {string.Join(", ", weapons)}");
+                            }
+
+                            bunkers.Dequeue();
+                            weapons.Clear();
+                            freeCapacity = maxCapacity;
+                        }
+
+                        if (!weaponContained && bunkers.Count == 1)
+                        {
+                            if (maxCapacity >= weaponCapacity)
+                            {
+                                if (freeCapacity < weaponCapacity)
+                                {
+                                    while (freeCapacity < weaponCapacity)
+                                    {
+                                        int removedWeapon = weapons.Dequeue();
+                                        freeCapacity += removedWeapon;
+                                    }
+                                }
+
+                                weapons.Enqueue(weaponCapacity);
+                                freeCapacity -= weaponCapacity;
+                            }
+                        }
                     }
+
                 }
+
+                input = Console.ReadLine();
             }
         }
     }
